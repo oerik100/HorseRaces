75a8be4137c1b91d2f8b59461e08aab244035662
Erik Olson <oerik100@gmail.com>
2023-02-22 09:22:45 -0500

    writing code for all aces to show up.  Only two are so I think CSS updates will be next

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
           console.log(`error ${err}`)
       });
 
+document.querySelector('#play').addEventListener('click', getFetch)
+
 
 function getFetch(){
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
@@ -24,13 +26,48 @@ function getFetch(){
           for(let i = 0; i < 51; i++){
             if(data.cards[i].code == 'AS'){
               console.log(i)
+              return i
             }
           }
         }
-        getSpadeCode()
+        let aceOfSpadeCode = getSpadeCode()
+
+        function getClubCode(){
+          for(let i = 0; i < 51; i++){
+            if(data.cards[i].code == 'AC'){
+              console.log(i)
+              return i
+            }
+          }
+        }
+        let aceOfClubCode = getClubCode()
+
+        function getHeartCode(){
+          for(let i = 0; i < 51; i++){
+            if(data.cards[i].code == 'AH'){
+              console.log(i)
+              return i
+            }
+          }
+        }
+        let aceOfHeartCode = getHeartCode()
+
+        function getDiamondCode(){
+          for(let i = 0; i < 51; i++){
+            if(data.cards[i].code == 'AD'){
+              console.log(i)
+              return i
+            }
+          }
+        }
+        let aceOfDiamondCode = getDiamondCode()
 
 
-        //document.querySelector('#aceOfSpades').src = aceSpade.image
+        document.querySelector('#aceOfSpades').src = data.cards[aceOfSpadeCode].image
+        document.querySelector('#aceOfClubs').src = data.cards[aceOfClubCode].image
+        document.querySelector('#aceOfSpades').src = data.cards[aceOfHeartCode].image
+        document.querySelector('#aceOfSpades').src = data.cards[aceOfDiamondCode].image
+
 
 
       })
